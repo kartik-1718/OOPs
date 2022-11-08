dacd02433c84d6114a28562381ac9f677dfee819
kartik-1718 <kakk36727@gmail.com>
2022-11-08 18:48:09 +0530

    Commit

---
+++ src/com/learn/collection/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.learn.collection;
+
+import java.util.ArrayList;
+
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> participant = new ArrayList<>();
+
+        participant.add("Nishant");//index 0
+        participant.add("Asif");//1
+        participant.add("Akash");//2
+        participant.add("Akshay");//3
+        participant.add("Amit");//4
+
+        System.out.println(participant);
+
+
+    }
+}
--- src/com/learn/date/DateDemo.java
+++ src/com/learn/date/DateDemo.java
@@ -1,16 +1,20 @@
 package com.learn.date;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DateDemo {
     public static void main(String[] args) {
         LocalDate date = LocalDate.now();
         LocalDate yesterday = date.minusDays(1);
         LocalDate tomorrow = date .plusDays(1);
+        LocalDateTime time = LocalDateTime.now();
 
 
         System.out.println(date);
         System.out.println("yestrday's date was "+ yesterday);
         System.out.println("Tomorrow's date is "+ tomorrow);
+
+        System.out.println(time);
     }
 }
