0088337a34c05b5acd62fc6f159a4d967a7d5cbd
kartik-1718 <kakk36727@gmail.com>
2022-11-08 17:51:05 +0530

    Commit

---
+++ src/com/learn/exceptionHandling/RiskyCar.java
@@ -0,0 +1,24 @@
+package com.learn.exceptionHandling;
+
+import java.io.IOException;
+
+
+class Car1{
+    void breakdown() throws IOException{
+        throw new IOException("This Car is broken down");
+    }
+}
+
+public class RiskyCar {
+    public static void main(String[] args) {
+
+
+        try {
+            Car1 c1 = new Car1();
+            c1.breakdown();
+        } catch (Exception e) {
+            System.out.println("Exception is handled " + e);
+        }
+        System.out.println("Continue.....");
+    }
+}
