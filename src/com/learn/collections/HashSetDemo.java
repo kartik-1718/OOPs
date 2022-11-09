fd0042b41037164816504715e2896d766fc85be4
kartik-1718 <kakk36727@gmail.com>
2022-11-09 18:05:15 +0530

    Commit

---
+++ src/com/learn/collections/HashSetDemo.java
@@ -0,0 +1,24 @@
+package com.learn.collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetDemo    {
+    public static void main(String[] args) {
+
+        HashSet<String> members= new HashSet<>();
+        members.add("aaaaaa");
+        members.add("bbbbbbbb");
+        members.add("ccccccc");
+        members.add("ccccccc");
+
+        members.remove("aaaaaa");
+
+        Iterator<String> itr= members.iterator();
+
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+    }
+}
